da4536c042b7c7037753b184a9fd9b078ecf10fc
Christian Frugard <frugardc@gmail.com>
2012-09-07 15:01:35 -0400

    insertion sort and linear search, chapter 2 page 22

---
+++ app/models/sort.rb
@@ -0,0 +1,29 @@
+class Sort
+
+	def self.insertion(arr)
+		arr.each_with_index do |key,index|
+			i = index - 1
+			puts "Before WHILE for #{key} - " + arr.join(",")
+			while i > -1 and arr[i] > key
+				puts "Since the key (#{key}) is less than the element to it's left (#{arr[i]})"
+				arr[i + 1] = arr[i]
+				puts "replace the key to the right with the key to the left"
+				i = i - 1
+				puts arr.join(",")
+			end
+			arr[i + 1] = key
+			puts arr.join(",")
+			puts "----"
+		end
+		return arr
+	end
+
+	def self.linear_search(arr,value)
+		index = 0
+		while index < arr.size - 1 and arr[index] != value
+			index +=1
+		end
+		return index
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Algo::Application.config.secret_token = '703010a7e579f7429671d6ca600b22893f9e8fe34a96d85d565231eec76edbdd1dec0b7586daa12b6e309e46f100fc158205a0f38bf7813b858af0fadc28450c'
